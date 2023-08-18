badf992dec3c4afec25cb4adcd53bfd7e9e32844
ggs4029 <ggs65429@gmail.com>
2023-08-18 16:27:41 +0900

    Entity 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "orders")
+public class Order {
+
+	@Id @GeneratedValue
+	@Column(name = "order_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+	private List<OrderItem> orderItems = new ArrayList<>();
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "delivery_id")
+	private Delivery delivery;
+
+	// 주문 시간
+	private LocalDateTime orderDate;
+	// 주문 상태 (ORDER, CANCEL)
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	//==연관관계 편의 메소드==//
+	public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
+
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
+}
